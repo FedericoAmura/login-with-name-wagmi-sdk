92a242ac306767db29c3ed452afa476e9fb46aad
FedericoAmura <federicoamura@gmail.com>
2024-05-20 19:10:56 -0300

    feat: register name page updated design and improvements

--- src/pages/registerName/registerName.tsx
+++ src/pages/registerName/registerName.tsx
@@ -6,9 +6,15 @@ import { normalize } from "viem/ens";
 import "./registerName.css";
 import useNavigation from "../../hooks/useNavigation";
 
+const publicClient = createPublicClient({
+  chain: sepolia,
+  transport: http()
+});
+
 export interface RegisterProps {}
 
 export function RegisterName({}: RegisterProps) {
+  const [loading, setLoading] = useState<boolean>(false);
   const [name, setName] = useState<string>("");
   const [validation, setValidation] = useState<"success" | "error" | string>("");
   const { goToRegisterAuthFlows, goToRoot } = useNavigation();
@@ -18,32 +24,36 @@ export function RegisterName({}: RegisterProps) {
   }
 
   const validateName = async (event: any) => {
-    event.preventDefault();
-    setValidation("");
+    try {
+      setLoading(true);
 
-    if (!name) {
+      event.preventDefault();
+      setValidation("");
+
+      if (!name) {
+        setValidation("error");
+        return;
+      }
+
+      const ensAddress = await publicClient.getEnsAddress({
+        name: normalize(name),
+      });
+
+      if (ensAddress) {
+        setValidation(ensAddress);
+      } else {
+        setValidation("error");
+      }
+    } catch (error) {
+      console.error(error);
       setValidation("error");
-      return;
-    }
-
-    const publicClient = createPublicClient({
-      chain: sepolia,
-      transport: http()
-    });
-
-    const ensAddress = await publicClient.getEnsAddress({
-      name: normalize(name),
-    });
-
-    if (ensAddress) {
-      setValidation(ensAddress);
-    } else {
-      setValidation("error");
+    } finally {
+      setLoading(false);
     }
   }
 
   return (
-    <div>
+    <div className="centered-column">
       <h1>Link your wallet address to a name</h1>
 
       <div>
@@ -52,14 +62,14 @@ export function RegisterName({}: RegisterProps) {
           Any naming system that has a <a
           href="https://github.com/FedericoAmura/login-with-name-wagmi-sdk/tree/main/lib/nameResolvers"
           target="_blank">NameResolver</a> in the connector will work<br />
-          For simplicity, and because several projects integrate with it, in this demo we will use ENS<br />
+          For simplicity, and because several projects integrate with it, in this demo we use ENS<br />
         </p>
         <h3>1. Go to <a href="https://app.ens.domains/" target="_blank">ENS</a> and register your name</h3>
         <p>Once your name is registered and linked to your address we can validate below if it can be correctly
           resolved</p>
         <button onClick={openENS}>Open ENS</button>
         <p style={{ color: 'red' }}>
-          <span>Note</span>
+          <span>Note:</span>
           {' '}this demo uses Sepolia network, you will have to use it in ENS too or we won't be able to find it.
         </p>
       </div>
@@ -67,8 +77,9 @@ export function RegisterName({}: RegisterProps) {
       <div>
         <h3>2. Validate it can be found (optional)</h3>
         <p>Once you have registered your ENS name, enter it here and validate if we can find it</p>
-        <form className="validation-form" onSubmit={validateName}>
+        <form className="centered-column" onSubmit={validateName}>
           <input
+            style={{ maxWidth: "300px" }}
             type="text"
             placeholder="vitalik.eth"
             value={name}
@@ -77,14 +88,18 @@ export function RegisterName({}: RegisterProps) {
               setName(e.target.value);
             }}
           />
-          <button style={{ marginTop: "24px" }} type="submit">Check name</button>
+          <button disabled={loading} style={{ marginTop: "24px" }} type="submit">{loading ? "Looking..." : "Find name"}</button>
         </form>
         {(validation && validation !== "error") && (
-          <p>Your name is linked to address {validation}! You are ready to continue ✅</p>
+          <p>
+            Your name is linked to address {validation}!<br />
+            You are ready to continue ✅
+          </p>
         )}
         {validation === 'error' && (
           <p>
-            We could not find your name. Did you create it already? Remember it has to be linked in ENS with Sepolia network 🕵
+            We could not find your name. Did you create it already?<br />
+            Remember it has to be linked in ENS with Sepolia network 🕵
           </p>
         )}
       </div>
@@ -94,7 +109,7 @@ export function RegisterName({}: RegisterProps) {
       </p>
 
       <div className="button-bar" style={{ marginTop: "64px" }}>
-        <button onClick={() => goToRoot()}>Back</button>
+        <button onClick={() => goToRoot()}>Go to Home</button>
         <button onClick={() => goToRegisterAuthFlows()}>Continue to Auth Flows</button>
       </div>
     </div>
