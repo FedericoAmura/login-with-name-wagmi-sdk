876e092823855652c76e4615148c15678694df23
FedericoAmura <federicoamura@gmail.com>
2024-06-12 17:29:28 +0200

    feat: improve connect modal

--- src/connect/connect.tsx
+++ src/connect/connect.tsx
@@ -21,22 +21,36 @@ export function Connect({}: ConnectProps) {
     return null;
   }
 
+  const copyWCUri = () => {
+    navigator.clipboard.writeText(connectionProps.wcUri);
+  }
+
   const { address, domainName, walletUri, wcUri } = connectionProps;
 
   return (
     <div className="modal" id="modal-connect">
       <div className="modal__overlay" tabIndex={-1}>
-        <div className="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-loading-title">
-          <h1 className="modal__title" id="modal-loading-title">{domainName}</h1>
-          <p className="modal__content">Address: {address}</p>
+        <div className="centered-column" role="dialog" aria-modal="true" aria-labelledby="modal-loading-title">
+          <h1 className="modal__title" id="modal-name-title">Waiting your wallet connection 🔗</h1>
 
-          <p>Or scan with a Wallet Connect compatible wallet:</p>
-          <img src={`https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${encodeURIComponent(wcUri)}`} alt="QR Code" />
+          <h2 className="modal__title" style={{ marginBottom: 0 }} id="modal-loading-title">{domainName}</h2>
+          <p className="modal__content" style={{ fontSize: "1em" }}>Address: {address}</p>
 
           {connectionProps.walletUri && (
-            <p>If the wallet website does not open, click <a href={walletUri} target="_blank">here</a>
-            </p>
+            <>
+              <p className="no-margin">Your wallet should have opened automatically</p>
+              <p className="no-margin">
+                If it does not open, click <a href={walletUri} target="_blank">here</a>
+              </p>
+            </>
           )}
+          <p className="no-margin">Or scan and connect with a Wallet Connect compatible wallet:</p>
+
+          <img className="simple-margin"
+               src={`https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${encodeURIComponent(wcUri)}`}
+               alt="QR Code" />
+          <button type="button" onClick={copyWCUri}>Copy WalletConnect URI</button>
+
         </div>
       </div>
     </div>
